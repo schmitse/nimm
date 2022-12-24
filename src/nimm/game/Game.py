e6a2f99409e51897f66cbb24b0f7fadb57fb162f
Sebastian Schmitt <sebastian.schmitt1@rwth-aachen.de>
2022-12-24 14:35:55 +0100

    updates to GUI for 1v1

--- src/nimm/game/Game.py
+++ src/nimm/game/Game.py
@@ -42,8 +42,6 @@ class NimmGUI(tk.Frame):
         self._p2 = p2
         self._board = board
         self._clicked_buttons = []
-        self.move = None
-
         self.window = root
 
         self._setup_window()
@@ -53,35 +51,53 @@ class NimmGUI(tk.Frame):
         assert len(self._clicked_buttons), 'Please select buttons. '
         rows = np.array([pair[0] for pair in self._clicked_buttons])
         cols = np.sort([pair[1] for pair in self._clicked_buttons])
-        assert np.all(rows == rows[0]), \
-            f'Error, you may only choose from one row! {rows}'
-        self.move = [rows[0], cols[0], cols[-1]]
-        print(self.move)
+        if not np.all(rows == rows[0]):
+            print(f'Error, you may only choose from one row! {rows}')
+            self._unclick_buttons()
+            return None
+        move = [rows[0], cols[0], cols[-1]]
         if self._board.turn():
-            if self._p1.is_human():
-                mv = self.move
-            else:
-                mv = []
-            mv = self._p1.push_gui(mv)
+            mv = self._p1.push_gui(move if self._p1.is_human() else [])
         else:
-            if self._p2.is_human():
-                mv = self.move
-            else:
-                mv = []
-            mv = self._p2.push_gui(mv)
-        print(self._board)
+            mv = self._p2.push_gui(move if self._p2.is_human() else [])
+        self._click_buttons(mv)
+        turntext = 'Player 1 Turn' if self._board.turn() else 'Player 2 Turn'
+        self.player_turn.config(text=turntext)
         if self._board.is_gameover():
-            print(f'Winner is {self._board.winner()}')
             label = tk.Label(text=f'Winner is {self._board.winner()}')
             label.grid(row=0, column=4)
+            replay_button = tk.Button(text='Replay', width=20, height=4,
+                                      bg='white', fg='black',
+                                      command=self._setup_window)
+            replay_button.grid(row=0, column=5)
         self._clicked_buttons.clear()
         return None
+
+    def _popup_error(self) -> None:
+        window = tk.Toplevel()
+        window.wm_title('Illegal Move!')
+        rows = np.array([pair[0] for pair in self._clicked_buttons])
+        label = tk.Label(window, text=f'You may only choose one row at a time but chose: {rows}')
+        label.grid(row=0, column=0)
+        btn = tk.Button(window, text='Alright!', command=window.destroy)
+        btn.grid(row=0, column=1)
+
+    def _click_buttons(self, move: list) -> None:
+        row = move[0]
+        start, stop = move[1:]
+        for j in range(start, stop + 1):
+            self._buttons[row * self._board._board.shape[1] + j]['state'] = 'disabled'
+
+    def _unclick_buttons(self) -> None:
+        self._popup_error()
+        for i, j in self._clicked_buttons:
+            self._buttons[i * self._board._board.shape[1] + j]['state'] = 'active'
+        self._clicked_buttons.clear()
 
     def _clicked(self, i, j) -> None:
         """ save and disable the button """
         self._clicked_buttons.append([i, j])
         self._buttons[i * self._board._board.shape[1] + j]['state'] = 'disabled'
-        print(self._clicked_buttons)
         return None
 
     def _setup_window(self) -> None:
@@ -89,9 +105,13 @@ class NimmGUI(tk.Frame):
         creates window with all buttons and texts
         depending on size of playing board
         """
+        self._board.reset()
+
         rows, columns = self._board._board.shape
-        print(self.window.winfo_geometry())
         msize = int(1150 / columns)
+
+        self.player_turn = tk.Label(text='Player 1 Turn')
+        self.player_turn.grid(row=0, column=2)
 
         b_sub = tk.Button(text='Submit Selection', width=20,
                           height=4, bg='white', fg='black',
@@ -113,7 +133,6 @@ class NimmGUI(tk.Frame):
                                 image=np.random.choice(self._images), bd=0,
                                 command=lambda i=i, j=j: self._clicked(i, j))
             if not (j > i * 2):
-                print(i + 1, j + rows - i - 1)
                 _button.grid(row=i + 1, column=j + rows - i - 1)
 
             self._buttons.append(_button)
